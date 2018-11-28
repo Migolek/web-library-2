1b4f19c1183b5532351b7fa696d527f37cfa2235
Piotr <niemczykpiotr13@gmail.com>
2018-11-28 14:58:55 +0100

    Add statistics

--- src/views/Dashboard/Admin/Statistics/index.jsx
+++ src/views/Dashboard/Admin/Statistics/index.jsx
@@ -1,15 +1,72 @@
 import React, { Component } from 'react';
 import TableListStatistics from '../../../../components/TableListStatistics';
+import TableListCategories from '../../../../components/TableListCategories';
 import './style.scss';
-class Statistics extends Component {
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class SimpleTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
     return (
-      <div className="employees">
-        <h3>Statystyki</h3>
-        <TableListStatistics />
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Statystyki filmów" />
+            <Tab label="Statystyki kategorii" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && 
+        <TabContainer>
+          <h3>Statystyki filmów</h3>
+          <TableListStatistics />
+        </TabContainer>}
+        {value === 1 && 
+        <TabContainer>
+          <h3>Statystyki kategorii</h3>
+          <TableListCategories />
+        </TabContainer>}
       </div>
     );
   }
 }
 
-export default Statistics;
+SimpleTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTabs);
