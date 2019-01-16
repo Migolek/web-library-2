c7cf189c045c8e76f728fc950b7c0b7b36a5950a
Piotr <niemczykpiotr13@gmail.com>
2019-01-16 13:27:36 +0100

    Add logo

--- src/views/LoginPage/LoginHeader.jsx
+++ src/views/LoginPage/LoginHeader.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from '../../assets/icons/library.png';
+import logo2 from '../../assets/icons/wypożyczalnia.png';
+import logo3 from '../../assets/icons/wypożyczalnia2.png';
 
 export default class LoginHeader extends Component {
   render() {
     return (
       <div className="login-header">
-        <img className="logo-img" src={logo} alt="logo"/>
-        <h1 className="login-title">Web library</h1>
+        <img className="logo-img" src={logo2} alt="logo"/>
       </div>
     )
   }
